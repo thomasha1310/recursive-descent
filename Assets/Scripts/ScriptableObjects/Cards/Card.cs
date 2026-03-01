bfbfd983279eb5ffab13fa39d26bd76d51e8cdb7
Thomas Ha <thomasha.1310@gmail.com>
2026-03-01 13:08:23 -0500

    Create card scriptable objects

--- Assets/Scripts/ScriptableObjects/Cards/Card.cs
+++ Assets/Scripts/ScriptableObjects/Cards/Card.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public abstract class Card : ScriptableObject
 {
-    [SerializeField] public string cardName;
+    public string cardName;
     public int energyCost;
     public string flavorText;
     public Sprite icon;
+    public CardRarity rarity;
     public abstract void PerformAction(Player player, Entity target, GameManager manager);
 }
