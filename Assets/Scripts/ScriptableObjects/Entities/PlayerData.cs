b8d38812b484a5b77b312e5857dc8a5cf100a234
chimbiwide <chimbiwide@A.pc>
2026-03-01 15:36:44 -0500

    ajlsjbhgahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- Assets/Scripts/ScriptableObjects/Entities/PlayerData.cs
+++ Assets/Scripts/ScriptableObjects/Entities/PlayerData.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Blueprint/PlayerData")]
 public class PlayerData : EntityData
 {
-    public List<CardData> startingHand;
     public List<CardData> startingDeck;
     public PlayerClass playerClass;
     public Sprite avatar;
